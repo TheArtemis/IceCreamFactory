8aa87376b733fabadc995de6686124566bb0ef00
TheArtemis <lordef01@gmail.com>
2024-06-11 16:56:10 +0200

    Removed old animations

--- src/views/IceCreamView.vue
+++ src/views/IceCreamView.vue
@@ -6,23 +6,8 @@ const router = useRouter();
 
 </script>
 <template>
-    <transition name="slide">
-        <CreateIceCreamCard />
-    </transition>
+    <CreateIceCreamCard />
+
 </template>
 
-<style scoped>
-.slide-enter-active {
-    animation: slide-in 0.5s ease forwards;
-}
-
-@keyframes slide-in {
-    0% {
-        transform: translateY(-100%);
-    }
-
-    100% {
-        transform: translateY(0);
-    }
-}
-</style>
+<style scoped></style>
