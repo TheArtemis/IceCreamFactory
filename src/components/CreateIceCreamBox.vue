9fcfde1e08d29e0841fa67614d2f50b32d1578f8
TheArtemis <lordef01@gmail.com>
2024-06-11 16:58:09 +0200

    Ice Cream cards work with backend now

--- src/components/CreateIceCreamBox.vue
+++ src/components/CreateIceCreamBox.vue
@@ -2,7 +2,26 @@
 import SmoothButton from '@/components/SmoothButton.vue';
 import TitleWithDescription from '@/components/TitleWithDescription.vue';
 import FavouriteIceCreamCard from '@/components/FavouriteIceCreamCard.vue'
-import IceCreamCard from './IceCreamCard.vue';
+import IceCreamCard from '@/components/IceCreamCard.vue';
+import { onMounted, ref } from 'vue';
+import { getAllIceCreams } from '@/api/iceCream';
+import { toSingleTokenList } from '@/common/objectProcessing';
+import { format, addPeriod } from '@/common/textFormatting';
+
+const iceCreams = ref(null);
+
+onMounted(async () => {
+    const iceCreamsDTO = await getAllIceCreams();
+    if (iceCreamsDTO.code == 1) {
+        iceCreams.value = iceCreamsDTO.icecreams;
+        console.log(iceCreamsDTO)
+        console.log(iceCreams.value)
+    }
+
+    else
+        console.error(iceCreamsDTO.message);
+})
+
 </script>
 
 <template>
@@ -13,17 +32,27 @@ import IceCreamCard from './IceCreamCard.vue';
             <SmoothButton class="create-button" button-color="#B9E8E2" button-text="create" size="big" />
         </div>
         <div class="ice-cream selection">
-
             <TitleWithDescription title="Enjoy our premium selection."
                 description="A curated set of our best Ice Creams. Don't waste time choosing, let our top notch Ice Creams choose you:" />
             <div class="favourites-grid">
                 <FavouriteIceCreamCard iceCreamID="13" />
-                <FavouriteIceCreamCard iceCreamID="13" />
-                <FavouriteIceCreamCard iceCreamID="13" />
+                <FavouriteIceCreamCard iceCreamID="14" />
+                <FavouriteIceCreamCard iceCreamID="15" />
             </div>
             <TitleWithDescription title="Or choose your own from our vast offer."
                 description="Our vast selection will leave you without words. " />
-            <IceCreamCard />
+
+
+            <div v-if="iceCreams == null">
+                <p>Loading...</p>
+            </div>
+            <div v-else class="icecream-selction-grid">
+                <div v-for="iceCream in iceCreams" :key="iceCream.id">
+                    <IceCreamCard :id="(iceCream.id).toString()" :title="format(iceCream.name)"
+                        :description="addPeriod(iceCream.description)"
+                        :tokens="toSingleTokenList(iceCream.flavors, iceCream.cone, iceCream.toppings)" />
+                </div>
+            </div>
 
         </div>
     </div>
@@ -33,7 +62,7 @@ import IceCreamCard from './IceCreamCard.vue';
 .icecream-page-box {
     padding-top: 20px;
     padding-bottom: 20px;
-    width: 780px;
+    width: 900px;
     padding: 50px;
     height: 3000px;
 }
@@ -55,11 +84,25 @@ import IceCreamCard from './IceCreamCard.vue';
     margin-top: 20px;
     overflow-x: auto;
     padding-bottom: 20px;
-
     -ms-overflow-style: none;
     scrollbar-width: none;
 
     .favourites-grid::-webkit-scrollbar {
+        display: none;
+    }
+}
+
+.icecream-selction-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    padding-top: 20px;
+    padding-bottom: 20px;
+    row-gap: 20px;
+
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    .icecream-selction-grid::-webkit-scrollbar {
         display: none;
     }
 }
