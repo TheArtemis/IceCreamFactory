87cd84b49c310066d7ad175f49c26dfb6eba7842
TheArtemis <lordef01@gmail.com>
2024-06-14 23:24:51 +0200

    Adding a clickable Token WIP

---
+++ src/components/ClickableIceCreamToken.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { IceCreamToken } from '@/components/IceCreamToken.vue';
+import { ref } from 'vue';
+
+const emit = defineEmits(['click']);
+
+const props = defineProps({
+    text: String,
+    color: String,
+    type: String
+});
+
+const active = ref(false);
+
+function handleClick() {
+    active.value = !active.value;
+    emit('click', active.value);
+}
+
+</script>
+
+<template>
+    <div class="clickable-icecream-token" @click="handleClick">
+        <IceCreamToken />
+    </div>
+</template>
